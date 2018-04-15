e0a87820f4b7bdb47c28a13d5864391e98b08308
bhavi289 <bhavi.c16@iiits.in>
2018-04-15 14:40:31 +0530

    geolocation added

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -10,17 +10,39 @@ declare var google: any;
 })
 export class HomePage {
 
+  
   @ViewChild('map') mapElement: ElementRef;
   map: any;
+  lat: any;
+  lng: any;
   markers = [];
 
-  constructor(public navCtrl: NavController, public platform: Platform, private geolocation: Geolocation) {
+  constructor(public navCtrl: NavController, public platform: Platform, public geolocation: Geolocation) {
     // platform.ready().then(() => {
     //   this.initMap();
     // });
   }
   ionViewDidLoad(){
-    this.initMap();
+    console.log('Starting Geolocation');
+
+    var options = {
+        enableHighAccuracy: true
+    };
+
+    this.geolocation.getCurrentPosition(options)
+    .then((position) => {
+        console.log('Geolocation successful');
+
+        // this.currentLocation = {
+            this.lat= position.coords.latitude,
+            this.lng= position.coords.longitude
+        // };
+
+        let query = '?lat=' + position.coords.latitude + '&lng=' + position.coords.longitude;
+
+        // this.updatePlaces(query);
+
+    })
   }
 
   initMap() {
@@ -34,12 +56,11 @@ export class HomePage {
  
     // this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
 
-
-    this.map = new google.maps.Map(this.mapElement.nativeElement, {
-      zoom: 7,
-      center: {lat: 41.85, lng: -87.65}
-    });
-
+    
+    // this.map = new google.maps.Map(this.mapElement.nativeElement, {
+    //   zoom: 7,
+    //   center: {lat: 28.6139, lng: 77.2090}
+    // });
 
     // this.geolocation.getCurrentPosition({ maximumAge: 3000, timeout: 5000, enableHighAccuracy: true }).then((resp) => {
     //   let mylocation = new google.maps.LatLng(resp.coords.latitude,resp.coords.longitude);
@@ -48,15 +69,46 @@ export class HomePage {
     //     center: mylocation
     //   });
     // });
+
+    // this.geo.getCurrentPosition().then( pos => {
+    //   this.lat = pos.coords.latitude;
+    //   this.lng = pos.coords.longitude;
+    // }).catch(err => console.log(err));
+    
+    // this.geolocation.getCurrentPosition().then(resp => {
+    //   var mylocation = new google.maps.LatLng(resp.coords.latitude,resp.coords.longitude);
+    //   this.map = new google.maps.Map(this.mapElement.nativeElement, {
+    //     zoom: 15,
+    //     center:  {lat: 28.6139, lng: 77.2090}
+    //   });
+    // })
     // let watch = this.geolocation.watchPosition();
     // watch.subscribe((data) => {
     //   this.deleteMarkers();
-    //   let updatelocation = new google.maps.LatLng(data.coords.latitude,data.coords.longitude);
+    //   // let updatelocation = new google.maps.LatLng(data.coords.latitude,data.coords.longitude);
     //   let image = 'assets/imgs/blue-bike.png';
-    //   this.addMarker(updatelocation,image);
+    //   // this.addMarker(updatelocation,image);
     //   this.setMapOnAll(this.map);
     // });
+
+    this.geolocation.getCurrentPosition({ maximumAge: 3000, timeout: 5000, enableHighAccuracy: true }).then((resp) => {
+      let mylocation = new google.maps.LatLng(resp.coords.latitude,resp.coords.longitude);
+      this.map = new google.maps.Map(this.mapElement.nativeElement, {
+        zoom: 15,
+        center: mylocation
+      });
+    });
+    let watch = this.geolocation.watchPosition();
+    watch.subscribe((data) => {
+      this.deleteMarkers();
+      let updatelocation = new google.maps.LatLng(data.coords.latitude,data.coords.longitude);
+      let image = 'assets/imgs/blue-bike.png';
+      this.addMarker(updatelocation,image);
+      this.setMapOnAll(this.map);
+    });
   }
+
+  
   addMarker(location, image) {
     let marker = new google.maps.Marker({
       position: location,
