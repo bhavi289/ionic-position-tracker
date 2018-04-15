af2793e824f42377cdf1a796babc9d180e983e52
bhavi289 <bhavi.c16@iiits.in>
2018-04-15 20:39:27 +0530

    check ionic-native Device

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,9 +1,7 @@
 import { Component, ViewChild, ElementRef } from '@angular/core';
 import { NavController, Platform } from 'ionic-angular';
-import * as firebase from 'Firebase';
 import { Geolocation } from '@ionic-native/geolocation';
 import { Device } from '@ionic-native/device';
-
 declare var google: any;
 
 @Component({
@@ -12,53 +10,54 @@ declare var google: any;
 })
 export class HomePage {
 
+  
   @ViewChild('map') mapElement: ElementRef;
   map: any;
+  lat: any;
+  lng: any;
   markers = [];
-  ref = firebase.database().ref('geolocations/');
 
-  constructor(public navCtrl: NavController,
-    public platform: Platform,
-    private geolocation: Geolocation,
-    private device: Device) {
-    platform.ready().then(() => {
+  constructor(public navCtrl: NavController, public platform: Platform, public geolocation: Geolocation, private device: Device) {
+    // platform.ready().then(() => {
+    //   this.initMap();
+    // });
+  }
+  ionViewDidLoad(){
+    // console.log('Starting Geolocation');
+
+    var options = {
+        enableHighAccuracy: true
+    };
+
+    this.geolocation.getCurrentPosition(options)
+    .then((position) => {
+        console.log('Geolocation successful');
+
+        // this.currentLocation = {
+            this.lat= position.coords.latitude,
+            this.lng= position.coords.longitude
+        // };
+
+        let query = '?lat=' + position.coords.latitude + '&lng=' + position.coords.longitude;
+
+        // this.updatePlaces(query);
+
+      })
       this.initMap();
-    });
-    this.ref.on('value', resp => {
-      this.deleteMarkers();
-      snapshotToArray(resp).forEach(data => {
-        if(data.uuid !== this.device.uuid) {
-          let image = 'assets/imgs/green-bike.png';
-          let updatelocation = new google.maps.LatLng(data.latitude,data.longitude);
-          this.addMarker(updatelocation,image);
-          this.setMapOnAll(this.map);
-        } else {
-          let image = 'assets/imgs/blue-bike.png';
-          let updatelocation = new google.maps.LatLng(data.latitude,data.longitude);
-          this.addMarker(updatelocation,image);
-          this.setMapOnAll(this.map);
-        }
-      });
-    });
+    
   }
 
   initMap() {
-    // this.map = new google.maps.Map(this.mapElement.nativeElement, {
-    //   zoom: 7,
-    //   center: {lat: 41.85, lng: -87.65}
-    // });
-    
-    this.geolocation.getCurrentPosition().then((resp) => {
+    this.geolocation.getCurrentPosition({ maximumAge: 3000, timeout: 5000, enableHighAccuracy: true }).then((resp) => {
       let mylocation = new google.maps.LatLng(resp.coords.latitude,resp.coords.longitude);
       this.map = new google.maps.Map(this.mapElement.nativeElement, {
         zoom: 15,
         center: mylocation
       });
     });
-    let watch = this.geolocation.watchPosition();
+    let watch = this.geolocation.watchPosition(); 
     watch.subscribe((data) => {
       this.deleteMarkers();
-      this.updateGeolocation(this.device.uuid, data.coords.latitude,data.coords.longitude);
       let updatelocation = new google.maps.LatLng(data.coords.latitude,data.coords.longitude);
       let image = 'assets/imgs/blue-bike.png';
       this.addMarker(updatelocation,image);
@@ -66,6 +65,7 @@ export class HomePage {
     });
   }
 
+  
   addMarker(location, image) {
     let marker = new google.maps.Marker({
       position: location,
@@ -74,50 +74,20 @@ export class HomePage {
     });
     this.markers.push(marker);
   }
-
+  
   setMapOnAll(map) {
     for (var i = 0; i < this.markers.length; i++) {
       this.markers[i].setMap(map);
     }
   }
-
+  
   clearMarkers() {
     this.setMapOnAll(null);
   }
-
+  
   deleteMarkers() {
     this.clearMarkers();
     this.markers = [];
   }
 
-  updateGeolocation(uuid, lat, lng) {
-    if(localStorage.getItem('mykey')) {
-      firebase.database().ref('geolocations/'+localStorage.getItem('mykey')).set({
-        uuid: uuid,
-        latitude: lat,
-        longitude : lng
-      });
-    } else {
-      let newData = this.ref.push();
-      newData.set({
-        uuid: uuid,
-        latitude: lat,
-        longitude: lng
-      });
-      localStorage.setItem('mykey', newData.key);
-    }
-  }
-
 }
-
-export const snapshotToArray = snapshot => {
-    let returnArr = [];
-
-    snapshot.forEach(childSnapshot => {
-        let item = childSnapshot.val();
-        item.key = childSnapshot.key;
-        returnArr.push(item);
-    });
-
-    return returnArr;
-};
